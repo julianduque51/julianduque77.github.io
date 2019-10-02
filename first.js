70e4d846bc1f48b2b7f4157f720e17f421bb72ac
julianduque51 <54089731+julianduque51@users.noreply.github.com>
2019-10-02 23:22:39 +0700

    Add files via upload

--- first.js
+++ first.js
@@ -1,1 +1,97 @@
-onclick="document.getElementById('demo').innerHTML = Date()"
+var onclick="document.getElementById('demo').innerHTML = Date()"
+
+  
+  var img = document.getElementById("myImg");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  
+
+  var span = document.getElementsByClassName("close")[0];
+  
+
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+
+  var modal = document.getElementById("myModal");
+  
+
+  var img = document.getElementById("pattaya");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  
+
+  var span = document.getElementsByClassName("close")[0];
+  
+
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+
+  var modal = document.getElementById("myModal");
+  
+
+  var img = document.getElementById("chiang mai");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  
+
+  var span = document.getElementsByClassName("close")[0];
+  
+
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+  var modal = document.getElementById("myModal");
+  
+
+  var img = document.getElementById("phuket");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  
+
+  var span = document.getElementsByClassName("close")[0];
+  
+
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+  var modal = document.getElementById("myModal");
+  
+
+  var img = document.getElementById("thailand");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  
+
+  var span = document.getElementsByClassName("close")[0];
+  
+
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
